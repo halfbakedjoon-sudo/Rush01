bdf89917517bacaa08b0d709ec35ed48d7aeb575
Joon Hoe Hiew <johiew@u81z10s04.42kl.edu.my>
2026-07-25 09:32:05 +0800

    make change to rush01.h

--- joon/includes/rush01.h
+++ joon/includes/rush01.h
@@ -13,15 +13,15 @@
 #ifndef RUSH01_H
 # define RUSH01_H
 
-int		parse(int argc, char **argv, int clues[24]);
+int		parse(int argc, char **argv, int clues[16]);
 int		**get_grid(int size);
 int		check_row_duplicates(int **grid, int row, int size);
 int		check_col_duplicates(int **grid, int col, int size);
 int		get_left(int *row, int size);
 int		get_right(int *row, int size);
-int		row_clue_ok(int **grid, int row, int size, int clues[24]);
-int		col_clue_ok(int **grid, int col, int size, int clues[24]);
-int		solve(int **grid, int size, int clues[24], int pos);
+int		row_clue_ok(int **grid, int row, int size, int clues[16]);
+int		col_clue_ok(int **grid, int col, int size, int clues[16]);
+int		solve(int **grid, int size, int clues[16], int pos);
 void	printf_grid(int **grid, int size);
 void	free_grid(int **grid, int size);
 void	ft_putchar(char c);
